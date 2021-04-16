97e8168c235090804119b30fc080dce8590eac1b
Александр <rubanov.web@gmail.com>
2021-04-16 12:13:35 +0300

    Добавлен код задач

--- js/script.js
+++ js/script.js
@@ -187,20 +187,98 @@
 
 //9. Дан массив с элементами 'Привет, ', 'мир' и '!'.Необходимо вывести на экран фразу 'Привет, мир!'.
 
-let mas = [];
-let str = "";
-let countItems = +prompt("Кол-во элементов в массиве:");
+// let mas = [];
+// let str = "";
+// let countItems = +prompt("Кол-во элементов в массиве:");
 
-if(countItems > 0 && countItems <= 20) {
-    for(let i = 0; i < countItems; i++) {
-        mas[i] = Math.ceil(Math.random() * 10);
-    }
-    for(let index in mas) {
-        console.log(mas[index] + '\n');
-        str += mas[index] + '\n';
-    }
-    alert(str);
+// if(countItems > 0 && countItems <= 20) {
+//     for(let i = 0; i < countItems; i++) {
+//         mas[i] = Math.ceil(Math.random() * 10);
+//     }
+//     for(let index in mas) {
+//         console.log(mas[index] + '\n');
+//         str += mas[index] + '\n';
+//     }
+//     alert(str);
+// }
+// else {
+//     console.log("Кол-во элементов в массиве должно быть > 0 и не больше 20!");
+// }
+
+/***** 10. Даны два массива: ['a', 'b', 'c'] и[1, 2, 3].Объедините их вместе ****/
+/**** 1-й вариант*/
+// let arrayOne = ['a', 'b', 'c'];
+// let arrayTwo = [1, 2, 3, 4, 5];
+// let arrayMix = [];
+
+// let str = "";
+// let i = 0;
+
+// for(let index in arrayOne) {
+//     arrayMix[i] = arrayOne[index];
+//     i++;
+// }
+// for(let index in arrayTwo) {
+//     arrayMix[i] = arrayTwo[index];
+//     i++;
+// }
+// for(let index in arrayMix) {
+//     str += arrayMix[index] + '\n';
+// }
+// alert(str);
+
+/**** 2-й вариант*/
+// let massLet = ['a', 'b', 'c'];
+// let massNum = [1, 2, 3];
+
+// for (let i = 0; i < massNum.length; i++) {
+
+//     massLet[massLet.length] = massNum[i];
+
+// }
+// console.log(massLet);
+
+let month = "";
+let numberDate;
+let dayWeek = "";
+
+let date = new Date();
+
+let arrayDays = {
+    0: "воскресенье",
+    1: "понедельник",
+    2: "вторник",
+    3: "среда",
+    4: "четверг",
+    5: "пятница",
+    6: "суббота"
 }
-else {
-    console.log("Кол-во элементов в массиве должно быть > 0 и не больше 20!");
+
+let arrayMonth = {
+    0: "январь",
+    1: "февраль",
+    2: "март",
+    3: "апрель",
+    4: "май",
+    5: "июнь",
+    6: "июль",
+    7: "август",
+    8: "сентябрь",
+    9: "октябрь",
+    10: "ноябрь",
+    11: "декабрь"
 }
+
+for (let index in arrayDays) {
+    if(date.getDay() == index) {
+        dayWeek = arrayDays[index];
+    }
+}
+
+for (let index in arrayMonth) {
+    if (date.getMonth() == index) {
+        month = arrayMonth[index];
+    }
+}
+
+console.log(`Сегодня - ${month}, ${date.getDate()} (${dayWeek})`);
