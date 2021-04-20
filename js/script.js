0c12877ff6c55462c36eda354c0fedbde1a446a4
Александр <rubanov.web@gmail.com>
2021-04-20 12:40:42 +0300

    Обновление кода

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 // Решение уравнения
-
-let inputs = document.querySelectorAll("input");
-
-// console.log(inputs);
+let inputs = document.querySelectorAll("input"); //коллекция полей (поля ввода и кнопки)
 
 let inputParamA = document.getElementById("param_a");
 let inputParamB = document.getElementById("param_b");
 let inputParamC = document.getElementById("param_c");
 
-let divButtons = document.querySelector(".buttons");
+let paramA; //значение коэф. а
+let paramB; //значение коэф. b
+let paramC; //значение коэф. c
 
-let paramA;
-let paramB;
-let paramC;
-let result;
+let btnCalc = document.getElementById("btn_calc"); //кнопка расчёта
+let btnReset = document.getElementById("btn_reset"); //кнопка очистки
 
-let btnCalc = document.getElementById("btn_calc");
-let btnReset = document.getElementById("btn_reset");
+let result; //результат вычисления
+let solution; //объект (параграф) для вывода результата
+let removed;
 
+// обработчик события "input" при вводе в поле коэф. a 
 inputParamA.addEventListener("input", () => {
     inputParamB.removeAttribute("disabled");
     
@@ -27,16 +26,18 @@ inputParamA.addEventListener("input", () => {
     paramA = +inputParamA.value;
 })
 
+// обработчик события "input" при вводе в поле коэф. b
 inputParamB.addEventListener("input", () => {
     inputParamC.removeAttribute("disabled");
     paramB = +inputParamB.value;
 })
 
+// обработчик события "input" при вводе в поле коэф. c
 inputParamC.addEventListener("input", () => {
     paramC = +inputParamC.value;
 })
 
-// Обработчик события "клик по кнопке Очистить"
+// обработчик события "click" при клике по кнопке "Очистить"
 btnReset.addEventListener("click", () => {
     for(let item of inputs) {
         if(item.getAttribute("type") == "number") {
@@ -49,18 +50,121 @@ btnReset.addEventListener("click", () => {
             item.setAttribute("disabled", "disabled");
         }
     }
+    removed = document.body.removeChild(solution);
 })
 
-// Обработчик события "клик по кнопке Произвести расчёт"
+// обработчик события "click" при клике по кнопке "Произвести расчёт"
 btnCalc.addEventListener("click", () => {
     result = calcSolution(paramA, paramB, paramC);
-    
     printSolution();
-
-    // let solution = document.createElement("p");
-    // solution.innerHTML = result;
-    // divButtons.append(solution);
 })
+
+// главная функция расчёта корней (вычисление)
+function calcSolution(a, b, c) {
+    let D; //дискриминант
+    let result; //строка с итогом
+
+    if (typeof b == "undefined") {
+        b = 0;
+    }
+    if (typeof c == "undefined") {
+        c = 0;
+    }
+
+    if(a == 0) {
+        if(b == 0) {
+            result = "Корней нет!";
+        }
+        else {
+            if(c != 0) {
+                result = -c / b;
+            }
+            else {
+                result = 0;
+            }
+        }
+    }
+    else if(b == 0) {
+        if(c != 0) {
+            (-c / a >= 0) ? result = Math.sqrt(-c / a) : result = "Корней нет!";
+        }
+        else {
+            result = 0;
+        }
+    }
+    else if(c == 0) {
+        result = [0, -b / a];
+    }
+    else {
+        D = calcD(a, b, c);
+        result = calcRoots(D, a, b, c);
+    }
+    return result;
+}
+
+// функция для расчёта дискриминанта
+function calcD(a, b, c) {
+    return b ** 2 - 4 * a * c;
+}
+
+// функция для расчёта корней квадратного уравнения
+function calcRoots(D, a, b, c) {
+    let x1, x2; //корни квадратного уравнения
+
+    if(D > 0) {
+        x1 = (-b + Math.sqrt(D)) / (2 * a);
+        x2 = (-b - Math.sqrt(D)) / (2 * a);
+
+        return [x1, x2];
+    }
+    else if (D == 0) {
+        return -b / (2 * a);
+    }
+    else {
+        return "Корней нет!";
+    }
+}
+
+// функция вывода результа на страницу (в объект p)
+function printSolution() {
+    if (removed) {
+        solution = createElem("p", "");
+        document.body.append(solution);
+
+        removed = null;
+    }
+    if (solution) {
+        solution.innerHTML = checkResult(result);
+    }
+    else {
+        solution = createElem("p", checkResult(result));
+        document.body.append(solution);
+    }
+}
+
+//функция создания элемента
+function createElem(tag, content) {
+    let elem;
+
+    elem = document.createElement(tag);
+    elem.innerHTML = content;
+
+    return elem;
+}
+
+// функция проверки результата вычисления
+function checkResult(result) {
+    if (typeof result == "string") {
+        return result;
+    }
+    else if (typeof result == "number") {
+        return `Уравнение имеет один корень: x = ${result.toFixed(2)}`;
+    }
+    else {
+        return "Уравнение квадратное. Имеет два корня:\n" + "x1 = " + result[0].toFixed(2) + "\nx2 = " + result[1].toFixed(2);
+    }
+}
+
 
 // let params = setParametrs();
 // let solution;
@@ -109,81 +213,14 @@ btnCalc.addEventListener("click", () => {
 //     }
 // }
 
-function calcSolution(a, b, c) {
-    let D; //дискриминант
-    let result; //строка с итогом
-
-    if (typeof b == "undefined") {
-        b = 0;
-    }
-    if (typeof c == "undefined") {
-        c = 0;
-    }
-
-    if(a == 0) {
-        if(b == 0) {
-            result = "Корней нет!";
-        }
-        else {
-            if(c != 0) {
-                result = -c / b;
-            }
-            else {
-                result = 0;
-            }
-        }
-    }
-    else if(b == 0) {
-        if(c != 0) {
-            (-c / a >= 0) ? result = Math.sqrt(-c / a) : result = "Корней нет!";
-        }
-        else {
-            result = 0;
-        }
-    }
-    else if(c == 0) {
-        result = [0, -b / a];
-    }
-    else {
-        D = calcD(a, b, c);
-        result = calcRoots(D, a, b, c);
-    }
-    return result;
-}
-
-function calcD(a, b, c) {
-    return b ** 2 - 4 * a * c;
-}
-
-function calcRoots(D, a, b, c) {
-    let x1, x2; //корни квадратного уравнения
-
-    if(D > 0) {
-        x1 = (-b + Math.sqrt(D)) / (2 * a);
-        x2 = (-b - Math.sqrt(D)) / (2 * a);
-
-        return [x1, x2];
-    }
-    else if (D == 0) {
-        return -b / (2 * a);
-    }
-    else {
-        return "Корней нет!";
-    }
-}
-
-function getSolution(solution) {
-    if(typeof solution == "string") {
-        alert(solution);
-    }
-    else if(typeof solution == "number") {
-        alert(`Уравнение имеет один корень: x = ${solution.toFixed(2)}`);
-    }
-    else {
-        alert("Уравнение квадратное. Имеет два корня:\n" + "x1 = " + solution[0].toFixed(2) + "\nx2 = " + solution[1].toFixed(2));
-    }
-}
-
-function printSolution() {
-
-}
+// function getSolution(solution) {
+//     if(typeof solution == "string") {
+//         alert(solution);
+//     }
+//     else if(typeof solution == "number") {
+//         alert(`Уравнение имеет один корень: x = ${solution.toFixed(2)}`);
+//     }
+//     else {
+//         alert("Уравнение квадратное. Имеет два корня:\n" + "x1 = " + solution[0].toFixed(2) + "\nx2 = " + solution[1].toFixed(2));
+//     }
+// }
