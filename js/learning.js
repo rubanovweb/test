668f33ffb7107aff27a133032645387863b7f147
Александр <rubanov.web@gmail.com>
2021-04-30 11:01:09 +0300

    Изучение классов и объектов

--- js/learning.js
+++ js/learning.js
@@ -1,123 +1,187 @@
-const input = {
-  type: "number",
-  placeholder: "Введите возраст",
-  minValue: 1,
-  styles: {
-    display: "flex",
-    margin: "20px auto 0",
-    padding: "5px 10px",
-    border: "2px solid gold",
-    outlineColor: "lightgreen",
-    fontFamily: "Verdana, sans-serif",
-    fontSize: "18px",
-    color: "blue"
-  },
-  getType() {
-    return this.type;
-  },
-  getPlaceholder() {
-    return this.placeholder;
-  },
-  getStyles() {
-    return this.styles;
+const form = document.getElementById("generateCard");
+
+class Card {
+  constructor(id, className, text, imgUrl) {
+    this.id = id;
+    this.className = className;
+    this.text = text;
+    this.imageSource = imgUrl;
   }
-}
 
-const inputAge = createElem("input");
-document.body.insertAdjacentElement("afterbegin", inputAge);
-inputAge.focus();
+  createCard() {
+    let card;
+    let p;
+    let img;
 
-let p; //абзац с результатом
-let error; //окно с ошибкой
+    card = document.createElement("div");
+    card.id = this.id;
+    card.className = "card";
+    card.classList.add(this.className);
 
-const result = () => {
-  if(document.getElementById("result")) {
-    p.innerHTML = `Вам: ${inputAge.value} ${getGrammaticYear(+inputAge.value)}`;
-  }
-  else {
-    p = createElem("p");
-    inputAge.insertAdjacentElement("afterend", p);
-  }
-}
+    form.insertAdjacentElement("afterend", card);
 
-inputAge.addEventListener("input", () => {
-  if(+inputAge.value > 0) {
-    if(document.querySelector(".error")) {
-        setTimeout(() => {
-          error.classList.remove("animate__backInDown");
-          error.classList.add("animate__backOutUp");
-        }, 500);
-        setTimeout(() => {error.remove();}, 3000);
+    p = document.createElement("p");
+    p.textContent = this.text;
+
+    card.append(p);
+
+    if(typeof this.imageSource != "undefined") {
+      img = document.createElement("img");
+      img.src = this.imageSource;
+      img.alt = "Фото";
+
+      card.prepend(img);
     }
-    result();
   }
-  else {
-    if(inputAge.value != "") {
-        error = createElem("div");
-        document.body.insertAdjacentElement("afterbegin", error);
+}
+
+const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur temporibus maxime doloremque minima? Odio quaerat, et at necessitatibus error animi, impedit libero sit, expedita nulla debitis. Quasi qui ipsa commodi.";
+let imgUrl = "";
+let card;
+
+const radioButtons = document.querySelectorAll("[type='radio']");
+
+for(let radio of radioButtons) {
+  
+  radio.addEventListener("input", () => {
+    if(document.getElementById("card")) {
+      console.log("Есть!");
     }
     else {
-      if(document.querySelector(".error")) {
-       setTimeout(() => {
-          error.classList.remove("animate__backInDown");
-          error.classList.add("animate__backOutUp");
-        }, 500);
-        setTimeout(() => {error.remove();}, 3000);
+      if(radio.value == "card-no-photo") {
+        imgUrl = undefined;
       }
+      else {
+        imgUrl = "https://picsum.photos/500/300";
+      }
+      card = new Card("card", radio.value, text, imgUrl);
+      card.createCard();
     }
-  }
-})
+  })
 
-function createElem(tagName) {
-  elem = document.createElement(tagName);
+}
 
-  switch(tagName) {
-    case "p": elem.setAttribute("id", "result");
+// const input = {
+//   type: "number",
+//   placeholder: "Введите возраст",
+//   minValue: 1,
+//   styles: {
+//     display: "flex",
+//     margin: "20px auto 0",
+//     padding: "5px 10px",
+//     border: "2px solid gold",
+//     outlineColor: "lightgreen",
+//     fontFamily: "Verdana, sans-serif",
+//     fontSize: "18px",
+//     color: "blue"
+//   },
+//   getType() {
+//     return this.type;
+//   },
+//   getPlaceholder() {
+//     return this.placeholder;
+//   },
+//   getStyles() {
+//     return this.styles;
+//   }
+// }
+
+// const inputAge = createElem("input");
+// document.body.insertAdjacentElement("afterbegin", inputAge);
+// inputAge.focus();
+
+
+// let p; //абзац с результатом
+// let error; //окно с ошибкой
+
+// const result = () => {
+//   if(document.getElementById("result")) {
+//     p.innerHTML = `Вам: ${inputAge.value} ${getGrammaticYear(+inputAge.value)}`;
+//   }
+//   else {
+//     p = createElem("p");
+//     inputAge.insertAdjacentElement("afterend", p);
+//   }
+// }
+
+// inputAge.addEventListener("input", () => {
+//   if(+inputAge.value > 0) {
+//     if(document.querySelector(".error")) {
+//         setTimeout(() => {
+//           error.classList.remove("animate__backInDown");
+//           error.classList.add("animate__backOutUp");
+//         }, 500);
+//         setTimeout(() => {error.remove();}, 3000);
+//     }
+//     result();
+//   }
+//   else {
+//     if(inputAge.value != "") {
+//         error = createElem("div");
+//         document.body.insertAdjacentElement("afterbegin", error);
+//     }
+//     else {
+//       if(document.querySelector(".error")) {
+//        setTimeout(() => {
+//           error.classList.remove("animate__backInDown");
+//           error.classList.add("animate__backOutUp");
+//         }, 500);
+//         setTimeout(() => {error.remove();}, 3000);
+//       }
+//     }
+//   }
+// })
+
+// function createElem(tagName) {
+//   elem = document.createElement(tagName);
+
+//   switch(tagName) {
+//     case "p": elem.setAttribute("id", "result");
               
-              elem.style.textAlign = "center";
-              elem.style.fontFamily = "Georgia, serif";
-              elem.style.fontSize = "22px";
-              elem.style.color = "green";
+//               elem.style.textAlign = "center";
+//               elem.style.fontFamily = "Georgia, serif";
+//               elem.style.fontSize = "22px";
+//               elem.style.color = "green";
 
-              elem.innerHTML = `Вам: ${inputAge.value} ${getGrammaticYear(+inputAge.value)}`;
-              break;
-    case "input": elem.setAttribute("type", input.getType());
-                  elem.setAttribute("placeholder", input.getPlaceholder());
+//               elem.innerHTML = `Вам: ${inputAge.value} ${getGrammaticYear(+inputAge.value)}`;
+//               break;
+//     case "input": elem.setAttribute("type", input.getType());
+//                   elem.setAttribute("placeholder", input.getPlaceholder());
 
-                  for(let key in input.getStyles()) {
-                    elem.style[key] = input.getStyles()[key];
-                  }
-                  break;
-    case "div": elem.classList.add("error", "animate__animated", "animate__backInDown");
-                elem.textContent = "Ошибка! Возраст не может быть < 0.";
-                break;
-  }
+//                   for(let key in input.getStyles()) {
+//                     elem.style[key] = input.getStyles()[key];
+//                   }
+//                   break;
+//     case "div": elem.classList.add("error", "animate__animated", "animate__backInDown");
+//                 elem.textContent = "Ошибка! Возраст не может быть < 0.";
+//                 break;
+//   }
   
-  return elem;
-}
+//   return elem;
+// }
 
-function getGrammaticYear(years) {
-  let result = "";
+// function getGrammaticYear(years) {
+//   let result = "";
 
-//1, 21, 31,... (кроме 11) - "год"
-//2 - 4, 22, 23, 24,... (кроме 12, 13, 14) - "года"
-//5 - 19, 20, 25 - 29, 30, 35 - 39, 40,... - "лет"
+// //1, 21, 31,... (кроме 11) - "год"
+// //2 - 4, 22, 23, 24,... (кроме 12, 13, 14) - "года"
+// //5 - 19, 20, 25 - 29, 30, 35 - 39, 40,... - "лет"
 
-  if(years % 10 == 1 && years % 100 != 11) {
-    result = "год";
-  }
-  else if(years % 10 >= 2
-           && years % 10 <= 4
-           && (years % 100 > 14 || years % 100 < 12)
-         ) 
-  {
-    result = "года";
-  }
-  else {
-    result = "лет";
-  }
-  return result;
-}
+//   if(years % 10 == 1 && years % 100 != 11) {
+//     result = "год";
+//   }
+//   else if(years % 10 >= 2
+//            && years % 10 <= 4
+//            && (years % 100 > 14 || years % 100 < 12)
+//          ) 
+//   {
+//     result = "года";
+//   }
+//   else {
+//     result = "лет";
+//   }
+//   return result;
+// }
 
 // let users = [];
 // let user;
